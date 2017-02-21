43447d91f8a34f1133d2b213bca1b77d6b07f003
JM Manalo <manalojuanmiguel@rocketmail.com>
2017-02-22 04:31:57 +0800

    2.3.1 and 2.3.2 Edited

--- cs131pa2_3_2.m
+++ cs131pa2_3_2.m
@@ -1,2 +1,20 @@
 # CS 131: Numerical Methods
 # Programming Exercise 2.3: I am Willing to Weight
+
+N = 15;
+k(1:N) = 10000;
+W = zeros(15, 1); W(1, 1) = 100;
+K = zeros(15, 15);
+for i = 2:N
+  W(i,1) = W(i-1,1) - 5;
+end
+for i = 1:N
+  K(i, i) = k(i);
+  if (i > 1)
+    K(i, i-1) = -k(i);
+  endif
+  if (i < N)
+    K(i, i+1) = -k(i+1);
+    K(i, i) = K(i, i) + k(i+1);
+  endif
+end
